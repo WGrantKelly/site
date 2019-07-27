1e1947160023694a4f6f9f64e7a79a5b168335af
WGrantKelly <w.grant.kelly@gmail.com>
2019-07-27 18:45:19 -0400

    Added more grid stuf

---
+++ src/About.js
@@ -0,0 +1,253 @@
+import React from 'react';
+
+export default class About extends React.Component{
+    render() {
+        return(
+            <div id="innerMainPage">
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                    
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                </div>
+                <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                    <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                    <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                    <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                    <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                    <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+                    <div class="row">
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    <div class = "col-1"></div>
+                    </div>
+
+
+
+                    
+                </div>
+        );
+    };
+}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MyNav from './MyNav'
+import About from './About'
 import './App.css';
 
 function App() {
@@ -38,18 +39,11 @@ function App() {
           <div class = "col-1"></div>
           <div class = "col-1"></div>
         </div>
-        <div class="row">
+        <div class="row" id="mainReplaceAblePage">
           <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
+          <div class = "col-10">
+            <About></About>
+          </div>
           <div class = "col-1"></div>
         </div>
         <div class="row">
@@ -66,6 +60,7 @@ function App() {
           <div class = "col-1"></div>
           <div class = "col-1"></div>
         </div>
+        
         <div class="row">
           <div class = "col-1"></div>
           <div class = "col-1"></div>
