d755a8768fc34b41fc074da87811ca3d90581375
WGrantKelly <w.grant.kelly@gmail.com>
2019-07-28 00:49:14 -0400

    made uge bhanges

--- src/App.js
+++ src/App.js
@@ -1,166 +1,224 @@
 import React from 'react';
-import MyNav from './MyNav'
-import About from './About'
 import './App.css';
 
-function App() {
-  return (
-    <div id="wrapper">
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-        </div>
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-10"><MyNav></MyNav></div>
-          <div class = "col-1"></div>
-        </div>
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-        </div>
-        <div class="row" id="mainReplaceAblePage">
-          <div class = "col-1"></div>
-          <div class = "col-10">
-            <About></About>
-          </div>
-          <div class = "col-1"></div>
-        </div>
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-        </div>
-        
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-        </div>
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-        </div>
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-        </div>
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-        </div>
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-        </div>
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-        </div>
-        <div class="row">
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-          <div class = "col-1"></div>
-        </div>
-    </div>
-  );
+export default class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: 1,
+      dividedNum: 1
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  handleScroll(event){
+    if((this.state.num/30)>this.state.dividedNum){
+      document.getElementById("mainBox").style.setProperty('--gradientColor', this.getRandomColor());
+      this.setState({
+        dividedNum : this.state.dividedNum+1
+      });
+    }
+    console.log(this.state.num);
+    console.log(this.state.dividedNum);
+    this.setState({
+      num : this.state.num+1
+    });
+  }
+
+
+  render () {
+    return (
+      <div id="wrapper">
+        <div className="bigBox">
+  
+            <div className="sideBox">
+                <div className="bannerBox">
+                        <div className="bio">
+  
+                        </div>
+                        <div className="biotail">
+                          <div className="biotailtriangles" id="triangleLeft">
+  
+                          </div>
+                          <div className="biotailtriangles" id="triangleRight">
+  
+                          </div>
+                        </div>
+                </div>                    
+            </div>
+            <div id="mainBox">
+                <div className = "mainContent">
+                    
+                    Hey Whatsup its me boi
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+                <div className = "mainContent">
+                    
+                    shut up lil bih
+  
+                </div>
+  
+            </div>
+        </div>            
+      </div>
+    );
+  }
 }
 
-export default App;
+
